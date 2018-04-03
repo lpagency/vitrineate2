eaac0898950caa81c5f66db1e47cb77e8f48facf
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-04-03 12:49:51 -0300

    error marcas

--- static/js/marcas.js
+++ static/js/marcas.js
@@ -5,7 +5,7 @@ $(window).load(function()
 		var test = $(this).attr("href").split("listado_productos?tag=");
 		test = test[1].toLowerCase().replace(" ","_").replace(" ","_").replace(" ","_").replace(" ","_").replace(" ","_");
 
-		$(this).attr("href", window.location.href+"/listado_productos?tag="+test);
+		$(this).attr("href", window.location.href+"listado_productos?tag="+test);
 		console.log($(this).attr("href"));
 	});
 });
