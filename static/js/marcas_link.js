1fb18a251786b1ac2d6655be59bf6e6d59053f7b
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-04-13 11:40:19 -0300

    marcas_link.js

--- static/js/marcas_link.js
+++ static/js/marcas_link.js
@@ -1,69 +1,84 @@
 var marcas_link = function()
 {
+    var link = window.location.href;
+    if(link.indexOf("localhost:8518/vitrineate2") > -1)
+    {
+        link = "http://localhost:8518/vitrineate2/";
+    }
+    if(link.indexOf("vitrineate.sites.ondev.today") > -1)
+    {
+        link = "http://vitrineate.sites.ondev.today/";
+    }
+    if(link.indexOf("vitrineate.cl") > -1)
+    {
+        link = "https://www.vitrineate.cl/";
+    }
+
     $(".link-marca").each(function()
     {
         var link = $(this).attr("href");
         var aux = $(this).attr("marca");
+
         switch(aux)
         {
             case "Paula Mar":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=paula_mar");
+                $(this).attr("href", link+"listado_productos?tag=paula_mar");
             break;
 
             case "Dionea":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=dionea");
+                $(this).attr("href", link+"listado_productos?tag=dionea");
             break;
 
             case "Rossaria Scarinci":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=rossaria_scarinci");
+                $(this).attr("href", link+"listado_productos?tag=rossaria_scarinci");
             break;
 
             case "Pili Saenz Joyas":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=pili_saenz_joyas");
+                $(this).attr("href", link+"listado_productos?tag=pili_saenz_joyas");
             break;
 
             case "Lisantino":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=lisantino");
+                $(this).attr("href", link+"listado_productos?tag=lisantino");
             break;
 
             case "Kuyen":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=kuyen");
+                $(this).attr("href", link+"listado_productos?tag=kuyen");
             break;
 
             case "Amimevistemimami":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=a_mi_me_viste_mi_mami");
+                $(this).attr("href", link+"listado_productos?tag=a_mi_me_viste_mi_mami");
             break;
 
             case "A Mi Me Viste Mi Mami":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=a_mi_me_viste_mi_mami");
+                $(this).attr("href", link+"listado_productos?tag=a_mi_me_viste_mi_mami");
             break;
 
             case "Stoff":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=stoff");
+                $(this).attr("href", link+"listado_productos?tag=stoff");
             break;
 
             case "Yambo":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=yambo");
+                $(this).attr("href", link+"listado_productos?tag=yambo");
             break;
 
             case "Mora":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=mora");
+                $(this).attr("href", link+"listado_productos?tag=mora");
             break;
 
             case "Villagran Villagran":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=villagran_villagran");
+                $(this).attr("href", link+"listado_productos?tag=villagran_villagran");
             break;
 
             case "Amelie Lingerie":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=amelie_lingerie");
+                $(this).attr("href", link+"listado_productos?tag=amelie_lingerie");
             break;
 
             case "Ttani":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=ttani");
+                $(this).attr("href", link+"listado_productos?tag=ttani");
             break;
 
             case "Sr. Vittorio":
-                $(this).attr("href", "{{ site_base }}/listado_productos?tag=srvittorio");
+                $(this).attr("href", link+"listado_productos?tag=srvittorio");
             break;
         }
     });
