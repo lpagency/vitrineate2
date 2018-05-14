c7942a8c9a037a3e4198b1dfa32be3cb07e63ea9
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-05-14 15:17:33 -0400

    banner nicolas sanchez

--- static/js/banners.js
+++ static/js/banners.js
@@ -49,47 +49,65 @@ function banners(tag)
         case "stoff":
             $(".listado_productos_marca12").removeClass("hidden");
         break;
+
         case "yambo":
             $(".listado_productos_marca13").removeClass("hidden");
         break;
+
         case "mora":
             $(".listado_productos_marca14").removeClass("hidden");
         break;
+
         case "paula_mar":
             $(".listado_productos_marca15").removeClass("hidden");
         break;
+
         case "villagran_villagran":
             $(".listado_productos_marca16").removeClass("hidden");
         break;
+
         case "dionea":
             $(".listado_productos_marca17").removeClass("hidden");
         break;
+
         case "amelie_lingerie":
             $(".listado_productos_marca18").removeClass("hidden");
         break;
+
         case "ttanti":
             $(".listado_productos_marca19").removeClass("hidden");
         break;
+
         case "srvittorio":
             $(".listado_productos_marca20").removeClass("hidden");
         break;
+
         case "zelk":
             $(".listado_productos_marca21").removeClass("hidden");
         break;
+
         case "torosanto":
             $(".listado_productos_marca22").removeClass("hidden");
         break;
+
         case "nyah":
             $(".listado_productos_marca23").removeClass("hidden");
         break;
+
         case "fibra_eyewear":
             $(".listado_productos_marca24").removeClass("hidden");
         break;
+
         case "sundara_accesorios":
             $(".listado_productos_marca25").removeClass("hidden");
         break;
+
         case "lolalola_shoes":
             $(".listado_productos_marca26").removeClass("hidden");
+        break;
+
+        case "nicolas_sanchez":
+            $(".listado_productos_marca27").removeClass("hidden");
         break;
     }
 }
