d26a4e5f3f3ce9902edd24cfecb9f70079d120ca
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-06-07 14:22:53 -0400

    new elements in the header

--- static/js/products.js
+++ static/js/products.js
@@ -1,5 +1,5 @@
 var base_url = $.environmentVar(
-    'http://localhost:8520/',
+    'https://apibodegas.ondev.today/',
     'https://apibodegas.ondev.today/',
     'https://apibodegas.loadingplay.com/');
 var checkout_url = $.environmentVar(
